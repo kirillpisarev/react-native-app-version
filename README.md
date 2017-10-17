f50565712d96347d7a0727badd86e40f229bd30e
Писарев Кирилл <k.pisarev@maximatelecom.ru>
2017-10-17 11:17:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Native App Version
+
+Bump react-native app version number on both android and ios parts and stores version number + change
+timestamp in a separate file for future use inside the app. For example on About page.
+
+### Usage
+
+``` js
+react-native-app-version -v 1.0.0
+
+```
---
+++ index.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+const execSync = require('child_process').execSync
+const fs = require('fs')
+
+const arguments = argv = require('optimist')
+	.usage('Bump react-native app version.\nUsage: react-native-app-version -v 1.0.1')
+	.demand('v')
+	.alias('v', 'version')
+	.alias('p', 'path')
+	.describe('v', 'Version number')
+	.default('p', './app/config/version.json')
+	.check(args => checkVersion(args.version))
+	.argv
+
+const version = arguments.version
+const versionCode = getVersionCode(version)
+
+if(hasUncommittedChanges())
+	throw new Error("Repository has unrecorded changes. Commit first and then exec the command again")
+
+try {
+	writeVersionToFile(arguments.path, version)
+	setiOSVersion(version, versionCode)
+	setAndroidVersion(version)
+	commit(version)
+	console.log('Success')
+} catch(err) {
+	console.log(err.toString())
+	rollback()
+}
+
+function checkVersion(version) {
+	const VERSION_PATTERN = /^\d+\.\d{1,2}\.\d{1,2}(\.\w+)?$/
+
+	if(!VERSION_PATTERN.test(version))
+		throw new Error("Please specify version divided by dot as a first parameter")
+}
+
+function getVersionCode(version) {
+	const versionCode = version
+		.split('.')
+		.slice(0, 3)
+		.reduce((summ, part, index) => {
+		return summ += parseInt(part, 10) * Math.pow(Math.pow(10, 2 - index), 2)
+	}, 0)
+
+	if(isNaN(versionCode))
+		throw new Error("Incorrect version")
+
+	return versionCode
+}
+
+function hasUncommittedChanges() {
+	const gitStatus = execSync('git status').toString()
+
+	return !/nothing to commit/.test(gitStatus)
+}
+
+function writeVersionToFile(path, version) {
+	fs.writeFileSync(path, `{
+		"code": "${version}",
+		"time": ${Date.now()}
+	}`)
+}
+
+function setiOSVersion(version, versionCode) {
+	execSync(`
+		cd ./ios &&
+		agvtool new-marketing-version ${version} &&
+		agvtool new-version -all ${versionCode} &&
+		cd ../ &&
+		git add .
+	`)
+}
+
+function setAndroidVersion(version) {
+	execSync(`
+		npm --no-git-tag-version version ${version}
+	`)
+}
+
+function commit(version) {
+	execSync(`
+		git commit -am "Up version to ${version}"
+	`)
+}
+
+function rollback() {
+	execSync(`git reset HEAD --hard`)
+}
